7f4a6ed5917bf6f91f89cc0ee7a69947192cd00b
Katelyn-Collier <katelyncollier14@gmail.com>
2022-08-25 15:41:39 -0500

    Add project files.

---
+++ OperatorsEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorsEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"17 divided by 4 is {quotient} remainder {remainder}");
+
+            }
+        }
+    }
+}
